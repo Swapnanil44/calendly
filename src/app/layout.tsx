e4ed4628784966ab8ac0e6457002ff1bad794930
Swapnanil Majumdar <adioticswapnanil@gmail.com>
2025-09-02 13:40:19 +0530

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={ cn("min-h-screen bg-background font-sans",`${geistSans.variable} ${geistMono.variable} antialiased`)}
@@ -31,5 +33,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
